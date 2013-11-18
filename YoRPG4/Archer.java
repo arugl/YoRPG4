649b162b5d9611b9fa57adb2d5dc80c711c99d72
ihuangda <ihuangda@users.noreply.github.com>
2013-11-17 21:32:13 -0500

    Update Archer.java

--- YoRPG4/Archer.java
+++ YoRPG4/Archer.java
@@ -12,7 +12,17 @@ return name;
 }
 
 public static String about(){
-return "Archers shoot arrows from bows. They use long-range attacks. Archers are able to avoid attacks from monsters because of the long-range combat";
+return "Archers shoot arrows from bows. They use long-range attacks. Archers are able to avoid attacks from monsters because of the long-range combat. Their special power is to use their crossbow for increased damage.";
+}
+//METHODS
+//prepares Archer for special power during the round, use crossbow for more damage
+public void specialize(){
+  strength = 120;
+}
+
+// prepares Archer for normal hit
+public void normalize(){
+  strength = 100; 
 }
 
 public void regenerate() {
