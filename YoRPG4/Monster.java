ac707d808da6da315903b7e99938dc2799ca8525
ihuangda <ihuangda@users.noreply.github.com>
2013-11-17 22:44:55 -0500

    Update Monster.java

--- YoRPG4/Monster.java
+++ YoRPG4/Monster.java
@@ -16,7 +16,7 @@ public void specialize(){}
 public void normalize(){}
 
 public static String about(){
-return "Monsters are mean. The monster mainly lives in the depths of the forest, but can also be living in caves, underground, etc..basically everywhere.. Be ruthless and have no mercy when facing these evil creatures.";
+return "Monsters are mean. They mainly live in the depths of the forest, but also in caves, underground, etc..basically everywhere.. Relatively easy to kill.";
 }
 
 public void regenerate() {
