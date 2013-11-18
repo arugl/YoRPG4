5b84babd18483c5fcd9827144421de526f739b3e
ihuangda <ihuangda@users.noreply.github.com>
2013-11-17 19:05:24 -0500

    Update Character.java

--- YoRPG4/Character.java
+++ YoRPG4/Character.java
@@ -20,18 +20,17 @@ life -= n;
 
 public int attack(Character c){
 int damage = (int)((strength * attack) - c.getDefense());
+
+if (damage < 0) damage = 0;
+
 c.life -= damage;
 return damage;
 }
 
-public void specialize(){
-defense -= 5;
-attack += 5;
+public abstract void specialize(){
 }
 
-public void normalize(){
-defense = 10;
-attack = 10;
+public abstract void normalize(){
 }
 
 public abstract void regenerate(); 
